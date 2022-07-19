bf86141b02cc0fbdc6d0fb09c814b604a1097ef6
Josh Navarro <joshua.navarro@g.austincc.edu>
2022-07-19 16:48:44 -0500

    First commint for rain alert app

---
+++ main.py
@@ -0,0 +1,27 @@
+from http.client import responses
+from urllib import response
+
+
+import requests
+
+
+api_key="29f2013083a11e82bc35e6604ce77fc3"
+
+end_point = "https://api.openweathermap.org/data/2.5/onecall"
+
+parameters = {
+    "lat": 30.267153,
+    "lon": -97.743057,
+    "exclude": "current,minutely,daily",
+    "appid": "29f2013083a11e82bc35e6604ce77fc3"
+}
+
+
+
+response = requests.get(end_point, params=parameters)
+response.raise_for_status()
+
+weather_data =response.json()
+
+
+print(weather_data)
